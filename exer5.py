f67dc194f253739b4f48de1d9b5865cdbb41c188
RenatoMC1 <111983813+RenatoMC1@users.noreply.github.com>
2022-10-26 12:06:42 -0300

    Add files via upload

---
+++ exer5.py
@@ -0,0 +1,11 @@
+dicionario = {'João':'1020', 'Maria': '1520', 'Paulo': '1220', 'Pedro': '1120'}
+
+backup = dicionario.copy()
+dicionario.clear()
+
+
+dicionario = {'coelho':'1020', 'daniel': '1520', 'fabio': '1220', 'carlos': '1120'}
+
+print(dicionario)
+print(backup)
+
