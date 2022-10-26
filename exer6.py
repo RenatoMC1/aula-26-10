27dd8bd5a79f898beb5523876da6088bc4632605
RenatoMC1 <111983813+RenatoMC1@users.noreply.github.com>
2022-10-26 12:30:25 -0300

    Add files via upload

---
+++ exer6.py
@@ -0,0 +1,15 @@
+import time
+
+for i in range(10, 0, -1):
+    print(i)
+    time.sleep(1)
+
+print("FELIZ ANO NOVO!")
+
+contador = 10
+while contador > 0:
+    print(contador)
+    time.sleep(1)
+    contador = contador - 1
+
+print("FELIZ ANO NOVO!")
